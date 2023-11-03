65d54523ead894802188e4a49ed9816c82d29d1d
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-11-03 14:04:45 -0300

    Fix repository interface, domain entitities only

--- src/modules/user/application/repository/user.repository.ts
+++ src/modules/user/application/repository/user.repository.ts
@@ -1,11 +1,9 @@
-import { CreateUserDto } from '../../controller/dto/create-user.dto';
-import { UpdateUserDto } from '../../controller/dto/update-user.dto';
 import { User } from '../../domain/user.domain';
 
 export interface IUserRepository {
-  save(createUserDto: CreateUserDto): Promise<User>;
+  save(user: User): Promise<User>;
   getAll(): Promise<User[]>;
   getById(id: number): Promise<User>;
   delete(id: number): Promise<boolean>;
-  update(id: number, updateUserDto: UpdateUserDto): Promise<User>;
+  update(id: number, user: User): Promise<User>;
 }
